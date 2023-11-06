0118d1c6ad0db267c4365d6957b676190eb543f4
CSComarch <rajkonkret660@gmail.com>
2023-11-06 13:35:30 +0100

    update formaty

--- formaty.py
+++ formaty.py
@@ -13,3 +13,31 @@ print(f"Witaj {user}, maasz teraz {wiek} lat.")  # f - f-string
 print(f"Witaj {wiek}, maasz teraz {user} lat.")  # f - f-string
 # Witaj 39, maasz teraz Tomek lat.
 # 13:20
+
+print("Używamy wersji pythona %i" % 3)
+print("Używamy wersji pythona %f" % 3.8)  # Używamy wersji pythona 3.800000
+print("Używamy wersji pythona %.1f" % 3.8)  # Używamy wersji pythona 3.8
+print("Używamy wersji pythona %.2f" % 3.8)  # Używamy wersji pythona 3.80
+print("Używamy wersji pythona %.0f" % 3.8)  # Używamy wersji pythona 4
+print("Używamy wersji pythona %.f" % 3.8)  # Używamy wersji pythona 4
+# wykonyje zaokrąglenie przy wypisywaniu
+print("Używamy wersji Python {}".format(wersja))  # Używamy wersji Python 3.900001
+print(f"Używamy wersji Python {wersja}")  # Używamy wersji Python 3.900001
+
+print(f"Używamy wersji Python {wersja}")
+print(f"Używamy wersji Python {wersja:.1f}")  # Używamy wersji Python 3.9
+print(f"Używamy wersji Python {wersja:.2f}")  # Używamy wersji Python 3.90
+print(f"Używamy wersji Python {wersja:.0f}")  # Używamy wersji Python 4
+
+print(f"{user:>10}")  # "     Tomek"
+print(f"{user:>20}")  # "               Tomek"
+print(f"{user:<20}")  # "Tomek               "
+print(f"{user:^10}")  # "  Tomek   "
+
+print(liczba)  # 134567112456
+print("Nasza duża liczba {:,}".format(liczba))  # Nasza duża liczba 134,567,112,456
+print("Nasza duża liczba {:,}".format(liczba).replace(",", "."))  # Nasza duża liczba 134.567.112.456
+print("Nasza duża liczba {:,}".format(liczba).replace(",", " "))  # Nasza duża liczba 134 567 112 456
+print(f"Liczba {liczba:,}".replace(",", " "))  # Liczba 134 567 112 456
+
+
