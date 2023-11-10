de42d0b547d8fd4638e7dbc1a2f34daf78bae5a4
CSComarch <rajkonkret660@gmail.com>
2023-11-10 09:04:29 +0100

    update kl4

--- kl4-Dom.py
+++ kl4-Dom.py
@@ -24,7 +24,24 @@ class Dom:
     def wyswietl_okna(self):
         print(f"Okna {self.__liczba_okien} szt")
 
+    def zmien_metraz(self):
+        wyn = int(input("Podaj nowy metraż"))
+        self.__metraz = wyn
+        self.wyswietl_metraz()
+
+    def zmien_okna(self):
+        wyn = int(input("Podaj ilosc okien"))
+        self.__liczba_okien = wyn
+        self.wyswietl_okna()
+
+    def zmien_kolor(self):
+        wyn = input("Podaj kolor")
+        self.__kolor = wyn
+        self.wyswietl_kolor()
+
 
 dom1 = Dom("Radek", 300, "biały", 17)
 dom1.wyswietl_okna()
 # dopisac metody w stylu zmien...
+dom1.zmien_metraz()
+dom1.zmien_kolor()
