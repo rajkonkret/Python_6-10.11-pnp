f9e4158fefc3b66b906dc084e787fadc22f81b92
CSComarch <rajkonkret660@gmail.com>
2023-11-10 10:50:03 +0100

    add kl9

---
+++ kl9.py
@@ -0,0 +1,15 @@
+class MyException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+try:
+    # print(6 / 0)
+    # raise TypeError('Bład typu')
+    raise MyException("komentarz")  # MyException: komentarz
+except ZeroDivisionError:
+    print("Nie dziel przez zero")
+except TypeError:
+    print("Bład typu")
+except MyException:
+    print("Wystąpił wyjątek MyException")
